6ac09830d93f1d9edd48e45efe407cf4b8c6006b
Soumili Chatterjee <riyachatterjee403@gmail.com>
2022-01-28 17:36:10 +0530

    Day2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import List from "./components/mobile";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const style={
+    marginLeft:"3%",
+
+  }
+  // return (
+  //   <div className="App">
+  //    <h1 style={style}>hello</h1>
+  //     <img alt="test" src={logo}/>
+  //   </div>
+  // );
+
+  // return <div className="App">
+  //   {[1,2,3,4,5].map((e)=>(
+  //      <Todo num={e}/>
+  //   ))}
+   
+    // </div>
+const arr1=["android","blackberry","iPhone","windows phone"];
+const title1="Mobile Operating System";
+const arr2=["Samsung","HTC","Micromax","Apple"];
+const title2="Mobile Manufacturers";
+const style1=["","","",""];
+const style2=["square","square","","circle"];
+
+return <div style={style}>
+ {<List key="list1"  title={title1} name={arr1} style={style1}/>}
+{<List key="list2" title={title2} name={arr2} style={style2} />}
+ 
+  
+  
+</div>
+
+
 }
+
+
+// function Todo({num}){
+//   return <h1>Todo:{num}</h1>
+// }
 
 export default App;
---
+++ src/components/list.jsx
@@ -0,0 +1,11 @@
+
+function ls({name,style}){
+  console.log(style);
+  return <ul>
+       { name.map((e,i)=>(
+         <li key={i} style={{listStyleType:style[i]}}>{e}</li>
+     ))}
+  </ul>
+}
+
+export default ls;
